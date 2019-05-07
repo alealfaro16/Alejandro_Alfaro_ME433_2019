6fa53fdae93e818e250c45d292b05c6c0829d137
alealfaro16 <48205459+alealfaro16@users.noreply.github.com>
2019-05-06 23:50:14 -0500

    Delete PIC32.h

--- PIC32.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIC32__H__
-#define PIC32__H__
-
-#include <xc.h>                     // processor SFR definitions
-#include <sys/attribs.h>            // __ISR macro
-
-void PIC32_Startup(void);
-
-#endif // PIC32__H__
