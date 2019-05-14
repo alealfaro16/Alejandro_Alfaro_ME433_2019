44673e9879d103cf8aad2565e52ea4e301dc8dbf
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2019-05-14 13:13:26 -0500

    Harmony code

---
+++ HWK_11/imu.h
@@ -0,0 +1,20 @@
+#ifndef IMU_H
+#define IMU_H
+
+#define SLAVE_ADDR 0b1101011
+#define SLAVE_ADDR_R 0b11010111
+#define SLAVE_ADDR_W 0b11010110
+#define WHO_AM_I 0x0F
+#define CTRL1_XL 0x10
+#define CTRL2_G 0x11
+#define CTRL3_C 0x12
+#define OUTZ_H_XL 0x2D
+#define OUTZ_L_XL 0x2C
+#define OUT_TEMP_L 0x20
+
+void initIMU(void);
+void I2C_read_multiple(unsigned char address, unsigned char reg, unsigned char * data, int length);
+int movingAvg(int *ptrArrNumbers, int *ptrSum, int pos, int len, int nextNum);
+
+
+#endif
