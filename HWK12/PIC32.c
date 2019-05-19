b391eaa0ef568dcdddd44fc1d7714a2febaff610
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2019-05-19 15:38:25 -0500

    Uploaded code

---
+++ HWK12/PIC32.c
@@ -0,0 +1,29 @@
+#include "PIC32.h"
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+
+
+void PIC32_Startup(void) {
+
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0;
+    TRISBbits.TRISB4 = 1;
+    LATAbits.LATA4 = 1;
+    
+    __builtin_enable_interrupts();
+
+}
